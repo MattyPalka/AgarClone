6d83483cafa9c388359cecce7b3e9dee432e144e
MattyPalka <mateuszpalka90@gmail.com>
2020-03-09 21:08:39 +0000

    added helmet

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ app.use(express.static(__dirname + '/public'))
 const socketio = require('socket.io')
 const expressServer = app.listen(3000)
 const io = socketio(expressServer)
+const helmet = require('helmet')
+app.use(helmet())
 
